642a72b05485b0e283d1bccb94f20b44ce88f41c
Micael Gomes <contato.micaeloficial@gmail.com>
2024-10-13 20:29:26 -0300

    feat(RepositoryFactory): Adicionando factory de Repository e adição de MemoriaRepositryFactory e BDRRepositoryFactory

---
+++ src/br/edu/iff/jogoforca/embdr/BDRRepositoryFactory.java
@@ -0,0 +1,40 @@
+package br.edu.iff.jogoforca.embdr;
+
+import br.edu.iff.jogoforca.RepositoryFactory;
+
+public class BDRRepositoryFactory implements RepositoryFactory {
+
+	private static BDRRepositoryFactory soleInstance;
+
+	public static BDRRepositoryFactory getSoleInstance() {
+		if (soleInstance == null) {
+			soleInstance = new BDRRepositoryFactory();
+		}
+		return soleInstance;
+	}
+
+	private BDRRepositoryFactory() {
+
+	}
+
+	@Override
+	public PalavraRepository getPalavraRepository() {
+		return BDRPalavraRepository.getSoleInstance();
+	}
+
+	@Override
+	public TemaRepository getTemaRepository() {
+		return BDRTemaRepository.getSoleInstance();
+	}
+
+	@Override
+	public RodadaRepository getRodadaRepository() {
+		return BDRRodadaRepository.getSoleInstance();
+	}
+
+	@Override
+	public JogadorRepository getJogadorRepository() {
+		return BDRJogadorRepository.getSoleInstance();
+	}
+
+}
