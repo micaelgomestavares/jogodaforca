46111c925c2e07a05849089c5ce3a148d654eb61
Natalia Anairam <natalia.anairam@gsuite.iff.edu.br>
2024-10-17 17:57:53 -0300

    feat(TemaRepository): adição de TemaRepository

---
+++ src/br/edu/iff/bancodepalavras/dominio/tema/TemaRepository.java
@@ -0,0 +1,19 @@
+package br.edu.iff.bancodepalavras.dominio.tema;
+
+import br.edu.iff.repository.RepositoryException;
+
+public interface TemaRepository {
+
+
+    public Tema getPorId(long id);
+
+    public Tema[] getPorNome(String nome);
+
+    public Tema[] getTodos();
+
+    public void inserir(Tema tema) throws RepositoryException;
+
+    public void atualizar(Tema tema) throws RepositoryException;
+
+    public void remover(Tema tema) throws RepositoryException;
+}
