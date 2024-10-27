0e02e04ecab8e1a4d6294befbdab09f5cbcc1e92
Natalia Anairam <natalia.anairam@gsuite.iff.edu.br>
2024-10-27 18:45:39 -0300

    feat(BDRTemaRepository): add exception e soleIntance

--- src/br/edu/iff/bancodepalavras/dominio/tema/embdr/BDRTemaRepository.java
+++ src/br/edu/iff/bancodepalavras/dominio/tema/embdr/BDRTemaRepository.java
@@ -2,39 +2,59 @@ package br.edu.iff.bancodepalavras.dominio.tema.embdr;
 
 import br.edu.iff.bancodepalavras.dominio.tema.Tema;
 import br.edu.iff.bancodepalavras.dominio.tema.TemaRepository;
+import br.edu.iff.bancodepalavras.dominio.tema.emmemoria.MemoriaTemaRepository;
 import br.edu.iff.repository.RepositoryException;
 
 public class BDRTemaRepository implements TemaRepository {
 
+    private static BDRTemaRepository soleInstance;
+
+    private BDRTemaRepository() {}
+
+    public static BDRTemaRepository getSoleInstance() {
+
+        if (soleInstance == null) {
+
+            soleInstance = new BDRTemaRepository();
+        }
+
+        return soleInstance;
+    }
 
     @Override
     public Tema getPorId(long id) {
-        return null;
+
+        throw new UnsupportedOperationException("Unimplemented method 'getPorId'");
     }
 
     @Override
     public Tema[] getPorNome(String nome) {
-        return new Tema[0];
+
+        throw new UnsupportedOperationException("Unimplemented method 'getPorNome'");
     }
 
     @Override
     public Tema[] getTodos() {
-        return new Tema[0];
+
+        throw new UnsupportedOperationException("Unimplemented method 'getTodos'");
     }
 
     @Override
     public void inserir(Tema tema) throws RepositoryException {
 
+        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
     }
 
     @Override
     public void atualizar(Tema tema) throws RepositoryException {
 
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
     }
 
     @Override
     public void remover(Tema tema) throws RepositoryException {
 
+        throw new UnsupportedOperationException("Unimplemented method 'remover'");
     }
 
     @Override
